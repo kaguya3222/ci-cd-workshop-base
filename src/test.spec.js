a76e003c522a4b201ae6cd5dccefce9c1905d8cf
kaguya3222 <maksimanisimov322@gmail.com>
2023-05-10 19:28:19 +0300

    added tests

---
+++ src/test.spec.js
@@ -0,0 +1,18 @@
+import { setupCounter } from './counter';
+
+describe('test', () => {
+  test('test', () => {
+    document.body.innerHTML = `
+      <div>
+        <button id="counter" type="button"></button>
+      </div>
+    `;
+
+    setupCounter(document.querySelector('#counter'));
+
+    document.querySelector('#counter').click();
+    expect(document.querySelector('#counter').innerHTML).toBe('count is 1');
+    document.querySelector('#counter').click();
+    expect(document.querySelector('#counter').innerHTML).toBe('count is 2');
+  });
+});
