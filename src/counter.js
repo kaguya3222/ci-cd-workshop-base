d7723416fb43c69a0a1c2c3ae329f73f963ef5b4
kaguya3222 <maksimanisimov322@gmail.com>
2023-05-15 21:30:14 +0300

    test

--- src/counter.js
+++ src/counter.js
@@ -2,7 +2,7 @@ export function setupCounter(element) {
   let counter = 0;
   const setCounter = (count) => {
     counter = count;
-    element.innerHTML = `count is ${counter}`;
+    element.innerHTML = `count is ${counter} 123`;
   };
   element.addEventListener('click', () => setCounter(counter + 1));
   setCounter(0);
